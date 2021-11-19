19ad748b4ea1982c8bd14089514c927121baca49
Akash Geethanjana <akashgeethanjana@gmail.com>
2021-11-20 02:20:05 +0530

    add static root

--- backend/settings.py
+++ backend/settings.py
@@ -196,7 +196,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = 'static/images'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
