abf77104b439894639466a95581a904f49b3b3c2
Akash Geethanjana <akashgeethanjana@gmail.com>
2021-11-20 02:18:52 +0530

    cors allow origins all

--- backend/settings.py
+++ backend/settings.py
@@ -204,10 +204,7 @@ MEDIA_ROOT = 'static/images'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Cloudinary credentials
